97e647d700af46e5086e4549468869217c5de834
sujitgore1 <sujitgore7736@gmail.com>
2023-07-02 22:20:30 +0530

    Initial project setup with sidebar and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // HomeModule
+  {
+    path:'',redirectTo:'home',pathMatch:'full'
+  },
+  
+  {
+    path:'home',loadChildren:()=>import('./home/home.module').then(mod=>mod.HomeModule)
+  },
+
+  // ShopModule
+  {
+    path:'shop',loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule)
+  },
+
+  // ProductsModule
+  {
+    path:'products',loadChildren:()=>import('./products/products.module').then(mod=>mod.ProductsModule)
+  },
+
+  // FeatureModule
+  {
+    path:'features',loadChildren:()=>import('./feature/feature.module').then(mod=>mod.FeatureModule)
+  },
+
+  // PagesModule
+  {
+    path:'pages',loadChildren:()=>import('./pages/pages.module').then(mod=>mod.PagesModule)
+  },
+
+  // BlogsModule
+  {
+    path:'blogs',loadChildren:()=>import('./blogs/blogs.module').then(mod=>mod.BlogsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './SharedContent/components/footer/footer.component';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './SharedContent/components/header/header.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'primeng/carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
